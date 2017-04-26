486fb1e0efc4ff1d0dc03a247b15d1fba1128c66
Rafahel <rafahelmello@gmail.com>
2017-04-26 12:02:37 -0300

    Analisador lexico criado para aula de Compiladores feito incicialmente em C++ e agora traduzido para C#

---
+++ Analisador Lexico cSharp/Escrita.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// Resolver problema relacionado a saída do arquivo.
+/// C:\Users\Rafahel\Desktop
+/// entrada.txt
+/// </summary>
+
+
+namespace Analisador_Lexico_cSharp {
+    class Escrita {
+        /// <summary>
+        ///  Continuar a implementação da classe de escrita.
+        /// </summary>
+        /// <param name="caminho"></param>
+        /// <param name="nome"></param>
+        public static void escreverUltimoSimbolo(String caminho, String nome,List<int> saidas, String entrada) {
+            try {
+                
+                System.IO.StreamWriter file = new System.IO.StreamWriter(caminho + "/" + "RESULTADO_" + nome, append: true);
+                file.Write(entrada + " -> ");
+                for (int i = 0; i < saidas.Count; i++) {
+                    if (saidas[i] <= 0) {
+                        continue;
+                    }
+                    file.Write(saidas[i] + " ");
+                }
+                file.Write("\n");
+                file.Close();
+            }
+            catch (Exception e) {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERRO: NÃO FOI POSSÍVEL ESCREVER PARA O ARQUIVO: " + caminho + "/" + "RESULTADO_" + nome);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(e.ToString());
+                //Console.WriteLine(ERRO.ToString());
+                Console.ResetColor();
+            }
+        }
+        public static void limpaArquivo(String caminho, String nome) {
+            try {
+                System.IO.StreamWriter file = new System.IO.StreamWriter(caminho + "/" + "RESULTADO_" + nome, append: false);
+                file.Write("");
+                file.Close();
+            }
+            catch (Exception e) {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERRO: NÃO FOI POSSÍVEL ESCREVER PARA O ARQUIVO: " + caminho + "/" + "RESULTADO_" + nome);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(e.ToString());
+                //Console.WriteLine(ERRO.ToString());
+                Console.ResetColor();
+            }
+        }
+
+        
+
+    }
+}
---
+++ Analisador Lexico cSharp/Leitor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace Analisador_Lexico_cSharp {
+    class Leitor {
+        private String caminho;
+        private List<String> texto =  new List<string>();
+        private System.IO.StreamReader file; 
+        public List<String> LerArquivo(String caminho, String nome) {
+            String conteudo;
+            this.caminho = caminho;
+            try {
+                this.file = new System.IO.StreamReader(caminho + "/" + nome);
+                while ((conteudo = this.file.ReadLine()) != null) {
+
+                    //Console.ForegroundColor = ConsoleColor.Cyan;
+                    //Console.WriteLine(conteudo);
+                    //Console.ResetColor();
+                    texto.Add(conteudo);
+                }
+                this.file.Close();
+                
+            }
+            catch (Exception ERRO) {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERRO: ARQUIVO NÃO EXISTENTE");
+                //Console.WriteLine(ERRO.ToString());
+                Console.ResetColor();
+                
+            }
+            
+            return this.texto;
+        }
+
+        public List<String> separador(List<String> texto) {
+            List<String> novaLista = new List<string>();
+
+            for (int i = 0; i < texto.Count; i++) {
+                for (int j = 0; j < texto[i].Split().Length; j++) {
+                    String temp = texto[i].Split()[j];
+                    novaLista.Add(temp);
+                    //Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    //Console.WriteLine(temp);
+                    //Console.ResetColor();
+                }
+                
+                
+            }
+         
+            return novaLista;
+        }
+
+        public static bool arquivoExiste(String caminho, String nome) {
+            try {
+                System.IO.StreamReader file = new System.IO.StreamReader(caminho + "/" + nome);
+                file.Close();
+                return true;
+            }
+            catch (Exception ERRO) {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Caminho ou arquivo inválido, tente novamente.");
+                Console.ResetColor();
+                return false;
+            }
+
+        }
+
+    }
+}
